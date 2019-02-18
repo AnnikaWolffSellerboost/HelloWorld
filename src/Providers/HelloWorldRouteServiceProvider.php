e9e7e26e9f3397c53ddf5e82f9e301848be60e6f
Annika Wolff <annika.wolff@sellerboost.de>
2019-02-18 15:56:30 +0100

    initial

---
+++ src/Providers/HelloWorldRouteServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: annikawolff
+ * Date: 18.02.19
+ * Time: 14:59
+ */
+
+namespace HelloWorld\Providers;
+
+
+use Plenty\Plugin\RouteServiceProvider;
+use Plenty\Plugin\Routing\Router;
+
+class HelloWorldRouteServiceProvider extends RouteServiceProvider
+{
+    public function map(Router $router)
+    {
+        $router->get('hello','HelloWorld\Controllers\ContentController@sayHello');
+    }
+}
